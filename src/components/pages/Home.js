c5712ec27534780750cb60ad8196f062f31a86e4
JihadSalameh <jihadsalameh.js@gmail.com>
2023-10-27 19:58:36 +0300

    small modifications

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ImageSlider from '../ImageSlider'
 import { SliderData } from '../SliderData'
+import ContactUs from './ContactUs'
 import './Home.css'
 
 function Home() {
   return (
-    <ImageSlider slides={SliderData} />
-  )
+    <div>
+      <ImageSlider slides={SliderData} />
+      <ContactUs />
+    </div>
+  );
 }
 
 export default Home
