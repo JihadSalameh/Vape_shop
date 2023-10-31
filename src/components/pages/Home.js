c63e7f68b67665b03eab57f0be7e95388307d109
JihadSalameh <jihadsalameh.js@gmail.com>
2023-10-31 09:49:15 +0200

    small edits to arrows on slider

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import ImageSlider from '../ImageSlider'
 import { SliderData } from '../SliderData'
-import ContactUs from './ContactUs'
 import './Home.css'
 
 function Home() {
   return (
-    <div>
+    <div className='home_container'>
       <ImageSlider slides={SliderData} />
-      <ContactUs />
     </div>
   );
 }
